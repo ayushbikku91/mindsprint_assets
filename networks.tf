26b2618ba4443a49491be1484895fd94f5e82825
Ayush Parth <54761726+ayushbikku91@users.noreply.github.com>
2023-11-30 10:39:36 +0530

    Create networks.tf
    Create networking

---
+++ networks.tf
@@ -0,0 +1,22 @@
+resource "google_compute_network" "vpc" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "my-subnet"
+  network       = google_compute_network.vpc.self_link
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_firewall" "firewall" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
